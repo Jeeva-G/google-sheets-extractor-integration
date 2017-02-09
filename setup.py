160a265e5712520fd2cebcb827338db2c133d39f
David Gwartney <david.gwartney@import.io>
2017-02-08 22:01:05 -0800

    Read version from file

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@
 Setup for Import.io Google Sheet Extractor Integration
 """
 from setuptools import setup
+import re
+
+PACKAGE = 'importio_gsei'
+VERSION_FILE = "importio_gsei/version.py"
+verstrline = open(VERSION_FILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSION_FILE,))
 
 setup(
     name='importio_gsei',
-    version='0.4.0',
+    version=version,
     url='http://github.io/import.io/google-sheets-extractor-integration',
     author='David Gwartney',
     author_email='david.gwartney@import.io',
